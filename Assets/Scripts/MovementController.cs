f1b103425736085564ca75c4dbb7c1d34c5636b4
Aleksandr Spiian <aleksandr.spiian@icloud.com>
2024-08-03 21:51:15 +0700

    set up player moving

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 namespace DefaultNamespace
 {
-    public class MovementController : MonoBehaviour
+    public class MovementController
     {
-        public void Move()
+        public void Move(Vector2 input, Transform transform, float speed)
         {
-            
+            var velocity = input.normalized * speed;
+            transform.position += (Vector3)velocity * Time.deltaTime;
         }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using DefaultNamespace;
+using InputHandler;
 using UnityEngine;
+using VContainer;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _speed;
+    
+    private MovementController _movementController;
+    private IInputHandler _inputHandler;
+
+    private bool _canMove;
+
+    private void Start()
     {
-        
+        _movementController = new MovementController();
     }
 
-    // Update is called once per frame
-    void Update()
+    [Inject]
+    public void Construct(IInputHandler inputHandler)
     {
+        _inputHandler = inputHandler;
+        _canMove = true;
+    }
+    
+    private void FixedUpdate()
+    {
+        if (!_canMove) return;
         
+        var input = _inputHandler.GetInput();
+        _movementController.Move(input, transform, _speed);
     }
 }
