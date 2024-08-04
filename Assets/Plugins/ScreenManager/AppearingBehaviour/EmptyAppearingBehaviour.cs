03233967cec01588e79585839cf49d2048a418a4
Aleksandr Spiian <aleksandr.spiian@icloud.com>
2024-08-04 20:03:36 +0700

    imported screen manager plugin

---
+++ Assets/Plugins/ScreenManager/AppearingBehaviour/EmptyAppearingBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using ScreenManager.Interfaces;
+
+namespace ScreenManager
+{
+    public class EmptyAppearingBehaviour : IAppearingScreenBehaviour
+    {
+        public IEnumerator SetActiveAsync(bool state)
+        {
+            yield break;
+        }
+    }
+}
---
+++ Assets/Plugins/ScreenManager/Interfaces/IAppearingScreenBehaviour.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+
+namespace ScreenManager.Interfaces
+{
+    public interface IAppearingScreenBehaviour
+    {
+        IEnumerator SetActiveAsync(bool state);
+    }
+}
