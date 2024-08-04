f58ef1c8c752be406f9ff88e5cccbd3fbef1b3f3
Aleksandr Spiian <aleksandr.spiian@icloud.com>
2024-08-04 22:00:40 +0700

    imported the rest files of screen manager plugin

---
+++ Assets/Scripts/Screens/MainManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Screens
+{
+    public class MainManager : MonoBehaviour
+    {
+        // This script can be on starter scene and to open first game scene
+    }
+}
